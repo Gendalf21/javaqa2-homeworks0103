9fd03cf4d7ec9eeb43bab975849ddfec0a968d0d
Александр Сальников <alexander211981@mail.ru>
2022-03-28 23:29:56 +0700

    работа с классами

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String passport;
+    public String phone;
+    public Boolean subscription;
+    public FormDate birthday;
+}
